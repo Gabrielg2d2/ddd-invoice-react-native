163e1e4a2621a7746f75187091a58fb9cf8fef02
Gabriel-g2d2 <gabriel.g2d2@gmail.com>
2022-12-27 01:02:01 -0300

    refactor: remove base-url

--- babel.config.js
+++ babel.config.js
@@ -1,17 +1,6 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    plugins: [
-      [
-        "module-resolver",
-        {
-          alias: {
-            "@assets": "./assets",
-            "@src": "./src",
-          },
-        },
-      ],
-    ],
     presets: ["babel-preset-expo"],
   };
 };
